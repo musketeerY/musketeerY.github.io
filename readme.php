e7819c2440646cecc4e16edec8926aa58240cae4
musketeerY <kavneendra@gmail.com>
2021-12-06 04:01:30 +0530

    test

---
+++ readme.php
@@ -0,0 +1,107 @@
+<?php
+/*
+README file for CMSimple5setup Testversion
+
+
+
+========================= 
+     [DE] Deutsch
+========================= 
+
+I N S T A L L A T I O N 
+----------------------- 
+1. Entpacken Sie die zip Datei in einen Ordner Ihres PC.
+2. Laden Sie alle Dateien und Ordner des Verzeichnisses, in dem auch diese readme.php liegt, auf Ihren Webserver hoch, in das Verzeichnis, in dem Ihre CMSimple Website laufen soll.
+
+Sie können Ihre neue Website nun in einem Browser aufrufen. Sie können sich 5 min lang mit dem Passwort "test" einloggen und das Passwort ändern.
+
+Nach Ablauf von 5 min nach dem Upload können Sie sich nicht mehr einloggen, Sie müssen Setup ausführen, um ein Passwort zu vergeben.
+
+S E T U P 
+--------- 
+Sie können das Passwort auch per Setup ändern: 
+
+1. Laden Sie die Datei ./setup/setupControl.php in das Rootverzeichnis Ihrer CMSimple Website (CMSimpleRoot, Zweitsprache, Subsite), damit wird das Setup aktiviert.
+2. Auf einigen älteren Servern müssen Sie möglicherweise die Datei setupControl.php beschreibbar machen (666), um Setup zu aktivieren.
+3. Rufen Sie die Datei setup.php (im CMSimpleRoot) mit einem Browser auf und setzen Sie Ihr Passwort.
+
+Sie haben für das Setup 5 min Zeit, danach müssen Sie die Datei setupControl.php erneut hochladen.
+
+Das Setup muss auch in Zweitsprachen und Subsites ausgeführt werden.
+
+Nach dem Setup wird die Datei ./setupControl.php automatisch gelöscht, und Setup ist deaktiviert. 
+
+S I C H E R H E I T S H I N W E I S 
+-----------------------------------
+Nach dem Setup sollten Sie sicherheitshalber die Datei ./setup.php noch einmal aufrufen. Sollte Setup noch aktiv sein, löschen Sie die Datei ./setupControl.php bitte per ftp.
+
+PASSWORT VERGESSEN? KEIN LOGIN MOEGLICH nach einem Update?
+
+Aktivieren Sie das Setup und rufen Sie die setup.php auf.
+
+Weitere Informationen erhalten Sie unter: https://cmsimple.org/
+
+
+
+
+
+========================= 
+     [EN] English
+========================= 
+
+I N S T A L L A T I O N 
+----------------------- 
+1. Unzip the zip file in a folder of your PC.
+2. Upload all files and folders of the folder, where also this readme.php is located, to your webserver, to the folder, where your CMSimple Website shall run.
+
+Now you can call your new website with a web browser . You can log in for 5 minutes with the password "test" and change the password.
+
+5 minutes after the upload, you can no longer log in. You have to run Setup to assign a password.
+
+S E T U P
+---------
+You also can change the password via setup:
+
+1. Upload the file ./setup/setupControl.php to the installation folder of your CMSimple website, and the setup mode will be activated.
+2. On some older servers maybe you have to make the setupControl.php writable (666) to activate Setup.
+3. Call the ./setup.php (in the CMSimpleRoot) with a web browser and set your password.
+
+You have 5 min for the setup, after that you have to upload the file setupControl.php again.
+
+The setup can (must) also be executed in secondary languages ​​and subsites.
+
+After setup, the ./setupControl.php file is automatically deleted and setup is deactivated. 
+
+S A F E T Y  N O T I C E 
+------------------------
+For safety, after setup you should call the ./setup.php file again. If setup is still active, please delete the file ./setupControl.php by ftp.
+
+FORGOTTEN PASSWORD? NO LOGIN POSSIBLE after an update?
+
+Activate the setup and call the setup.php (see S E T U P)
+
+Further informations you get under: https://cmsimple.org/
+
+
+
+========================= 
+     END of README 
+========================= 
+*/
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<title>CMSimple5setup readme file</title>
+</head>
+<body>
+<p style="text-align: center; margin: 60px 0 20px 0;">
+Please open this file offline with a text editor, to get the informations.
+</p>
+<p style="text-align: center; margin: 0;">
+Bitte &ouml;ffnen Sie diese Datei offline mit einem Texteditor, um die Informationen zu erhalten.
+</p>
+</body>
+</html>
